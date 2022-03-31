cc27fe7a1a05eee681f5439d1d00df891bd89514
stanely1 <sb.reda456@gmail.com>
2022-03-31 23:54:55 +0200

    Update prac1

--- pracownia/prac1/solution.rkt
+++ pracownia/prac1/solution.rkt
@@ -48,20 +48,67 @@
 ; Wstawianie
 
 (define (table-insert row tab)
-  ;; uzupełnij
-  )
+  (define (valid? row col)
+    (cond [(null? row) (null? col)] ; ten sam rozmiar
+          [(null? col) (null? row)]
+          [else (and
+                 (cond [(equal? (column-info-type (car col)) 'number) (number? (car row))]
+                       [(equal? (column-info-type (car col)) 'string) (string? (car row))]
+                       [(equal? (column-info-type (car col)) 'symbol) (symbol? (car row))]
+                       [(equal? (column-info-type (car col)) 'boolean) (boolean? (car row))])
+                 (valid? (cdr row) (cdr col)))])) ; zgodnosc typow
+  (if (valid? row (table-schema tab))
+      (table (table-schema tab) (cons row (table-rows tab)))
+      (error 'table-insert "invalid row!")))
 
 ; Projekcja
 
 (define (table-project cols tab)
-  ;; uzupełnij
-  )
+  (define (filter-row row col which)
+    (cond [(or (null? which) (null? col)) null]
+          [(equal? (column-info-name (car col)) (car which))
+           (cons (car row) (filter-row (cdr row) (cdr col) (cdr which)))]
+          [else (filter-row (cdr row) (cdr col) which)]))
+  (define (filter-all rows)
+    (if (null? rows)
+        null
+        (cons (filter-row (car rows) (table-schema tab) cols)
+              (filter-all (cdr rows)))))
+  (define (make-schema cols which)
+    (cond [(or (null? cols) (null? which)) null]
+          [(equal? (column-info-name (car cols)) (car which))
+           (cons (column-info (car which) (column-info-type (car cols)))
+                 (make-schema (cdr cols) (cdr which)))]
+          [else (make-schema (cdr cols) which)]))
+  (table (make-schema (table-schema tab) cols) (filter-all (table-rows tab))))
 
 ; Sortowanie
 
 (define (table-sort cols tab)
-  ;; uzupełnij
-  )
+  (define (cmp row1 row2 col key) ; porownuje po jednnym kluczu (1 - r1 < r2 ; 0 - r1 = r2 ; -1 - r1 > r2
+    (cond [(null? col) 1]
+          [(equal? (column-info-name (car col)) key)
+           (cond [(equal? (car row1) (car row2)) 0]
+                 [(equal? (column-info-type (car col)) 'number)
+                  (if (< (car row1) (car row2)) 1 -1)]
+                 [(equal? (column-info-type (car col)) 'string)
+                  (if (string<? (car row1) (car row2)) 1 -1)]
+                 [(equal? (column-info-type (car col)) 'symbol)
+                  (if (symbol<? (car row1) (car row2)) 1 -1)]
+                 [(equal? (column-info-type (car col)) 'boolean)
+                  (if (car row1) -1 1)])]
+          [else (cmp (cdr row1) (cdr row2) (cdr col) key)]))
+  
+  (define (compare row1 row2 keys) ; po kolei porownuje klucze
+    (if (null? keys)
+        #f
+        (let [(x (cmp row1 row2 (table-schema tab) (car keys)))]
+          (cond [(= x 1) #t]
+                [(= x 0) (compare row1 row2 (cdr keys))]
+                [else #f]))))
+    
+  (table (table-schema tab)
+         (sort (table-rows tab) (lambda (x y) (compare x y cols)))))
 
 ; Selekcja
 
@@ -73,23 +120,106 @@
 (define-struct lt-f (name val))
 
 (define (table-select form tab)
-  ;; uzupełnij
-  )
+  (define (get-col-num name) ; numer kolumny
+    (define (it col num)
+      (if (equal? (column-info-name (car col)) name)
+          num
+          (it (cdr col) (+ 1 num))))
+    (it (table-schema tab) 0))
+
+  (define (cmp val1 val2 type) ; porownaie val1 < val2 w zaleznosci of typu
+    (cond [(equal? type 'number) (< val1 val2)]
+          [(equal? type 'string) (string<? val1 val2)]
+          [(equal? type 'symbol) (symbol<? val1 val2)]
+          [(equal? type 'boolean) (if val1 #f val2)]))
+    
+  (define (check row f) ; sprawdzenie pojedynczego wiersza
+    (cond [(lt-f? f)
+           (let [(num (get-col-num (lt-f-name f)))]
+             (cmp (list-ref row num) (lt-f-val f)
+                  (column-info-type (list-ref (table-schema tab) num))))]
+          [(eq-f? f)
+           (let [(num (get-col-num (eq-f-name f)))]
+             (equal? (list-ref row num) (eq-f-val f)))]
+          [(eq2-f? f)
+           (let [(num1 (get-col-num (eq2-f-name f)))
+                 (num2 (get-col-num (eq2-f-name2 f)))]
+             (equal? (list-ref row num1) (list-ref row num2)))]
+          [(not-f? f) (not (check row (not-f-e f)))]
+          [(or-f? f) (or (check row (or-f-l f)) (check row (or-f-r f)))]
+          [(and-f? f) (and (check row (and-f-l f)) (check row (and-f-r f)))]))
+
+  (table (table-schema tab)
+         (filter (lambda (x) (check x form)) (table-rows tab))))
+    
 
 ; Zmiana nazwy
 
 (define (table-rename col ncol tab)
-  ;; uzupełnij
-  )
+ (define (make-cols cols)
+   (if (null? cols)
+       null
+       (cons
+        (if (equal? (column-info-name (car cols)) col)
+            (column-info ncol (column-info-type (car cols)))
+            (car cols))
+        (make-cols (cdr cols)))))
+  (table (make-cols (table-schema tab)) (table-rows tab)))
 
 ; Złączenie kartezjańskie
 
 (define (table-cross-join tab1 tab2)
-  ;; uzupełnij
-  )
+  (define (make-rows rows1 rows2)
+    (cond [(null? rows1) null]
+          [(null? rows2) (make-rows (cdr rows1) (table-rows tab2))]
+          [else (cons (append (car rows1) (car rows2)) (make-rows rows1 (cdr rows2)))]))
+  (table (append (table-schema tab1) (table-schema tab2))
+         (make-rows (table-rows tab1) (table-rows tab2))))
 
 ; Złączenie
 
 (define (table-natural-join tab1 tab2)
-  ;; uzupełnij
-  )
+  (define (symbol-append a b)
+    (string->symbol (string-append (symbol->string a) (symbol->string b))))
+
+  (define (find-col cols name)
+    (cond [(null? cols) #f]
+          [(equal? (column-info-name (car cols)) name) #t]
+          [else (find-col (cdr cols) name)]))
+
+  (define (rep-list cols1 cols2)
+    (cond [(null? cols2) null]
+          [(find-col cols1 (column-info-name (car cols2)))
+           (cons
+            (cons (column-info-name (car cols2))
+                  (symbol-append (column-info-name (car cols2)) '_))
+            (rep-list cols1 (cdr cols2)))]
+          [else (rep-list cols1 (cdr cols2))]))
+
+  (define (rename-rep cols tab)
+    (if (null? cols)
+        tab
+        (table-rename (caar cols) (cdar cols) (rename-rep (cdr cols) tab))))
+
+  (define (select-rep-eq cols tab)
+    (if (null? cols)
+        tab
+        (table-select (eq2-f (caar cols) (cdar cols))
+                      (select-rep-eq (cdr cols) tab))))
+  
+  (define (no-rep-list rlist cols)
+    (cond [(null? cols) null]
+          [(and (not (null? rlist))
+                (equal? (column-info-name (car cols)) (caar rlist)))
+           (no-rep-list (cdr rlist) (cdr cols))]
+          [else (cons (column-info-name (car cols))
+                      (no-rep-list rlist (cdr cols)))]))
+
+  (letrec [(s1 (table-schema tab1))
+           (s2 (table-schema tab2))
+           (rlist (rep-list s1 s2))]
+    (table-project (append (map column-info-name s1)
+                           (no-rep-list rlist s2))
+                   (select-rep-eq rlist
+                                  (table-cross-join tab1
+                                                    (rename-rep rlist tab2))))))
